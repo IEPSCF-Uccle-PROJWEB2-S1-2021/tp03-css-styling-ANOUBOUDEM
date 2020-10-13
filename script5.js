316d3ff87f3e69c9c8595bd5fdc6a484285cdb20
CHAMBERLIN <anch1609@student.iepscf-uccle.be>
2020-10-13 05:31:39 +0200

    TP5

--- script5.js
+++ script5.js
@@ -1,5 +1,9 @@
-function Change(int_cod)
-{
-  let valeur;
-  return String.fromCharCode(int_cod);
+function textToBin(text) {
+  let length = text.length,
+      output = [];
+  for (let i = 0;i < length; i++) {
+    let bin = text[i].charCodeAt().toString(2);
+    output.push(Array(8-bin.length+1).join("0") + bin);
+  }
+  return output.join(" ");
 }
